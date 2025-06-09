0978c2958aef980c292a2bec737b1def97386ea0
vxtxcx <victorvicencio932@gmail.com>
2025-06-09 00:42:38 -0400

    Initial commit

---
+++ src/app/components/header.tsx
@@ -0,0 +1,7 @@
+
+import "./header.css"
+import App from "next/app";
+
+
+
+export default App
---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+
+
+export default function Home() {
+  return (
+    <div className="grid-container">
+      <header className="header">
+        <p>hola soy el header</p>
+      </header>
+      <main className="main">
+        <p>hola soy el main</p>
+      </main>
+      <footer className="footer">
+        <p>hola soy el footer</p>
+      </footer>
+    </div>
+  );
+}
